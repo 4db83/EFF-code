0509666fd28b9d2b4e9f96b8b10e3c7f646f5ae4
4db83 <84711454+4db83@users.noreply.github.com>
2022-11-03 10:53:50 +0100

    add lines

--- california_test_scores.m
+++ california_test_scores.m
@@ -1,11 +1,10 @@
 clc; clear;
 % set path to local helper functions
-addpath(genpath('.\helper_functions')) 
+addpath(genpath('./helper_functions')) 
 % read in the data
-dt = readtable('.\data/caschool.xlsx');
+dt = readtable('./data/caschool.xlsx');
 % inspec the data that was loaded
-head(dt)
-tail(dt)
+head(dt);tail(dt);
 
 %% this is the main part that 
 y = dt.testscr;
@@ -15,17 +14,23 @@ xg = linspace(10,30,1e3)';
 xg = [ones(length(xg),1) xg];
 
 fprintf([repmat('=',1,90) '\n']);
-fprintf( 'Dependent Variable: Test Score\n')
+fprintf('\t\t\t\t\t Dependent Variable: Test Score\n')
 % the is the call to the OLS function
 fit1 = ols(y,x,[],[],0);
 
-clf
-hold on;
+clf; % clear old figures, if exis;
+hold on; % to plot multiple lines on top
   scatter(x,y,33,clr(1),'filled')
-  plot(xg,xg*fit1.bhat,'LineWidth',2)
+  plot(xg,xg*fit1.bhat,'LineWidth',2,'Color',clr(2))
   xlim([10 30])
-  set(gca,'Position',[.06 .4 .9 .3],'FontName','Times','FontSize',14)
-hold off
+  vline(mean(x),'k-')
+  hline(mean(y),'k-')
+hold off;
+grid on; 
+% adjust gca (current figure/axis) position, font, grid properties
+set(gca,'Position',[.06 .4 .9 .3],...
+        'FontName','Times','FontSize',14, ...
+        'GridLineStyle',':','GridAlpha',1/3)
 ylabel('Test Score');
 xlabel('Student-Teacher-Ratio')
 box on;
