b5b1a4c672e8348baa1d2de2ff756f9da05d247e
4db83 <84711454+4db83@users.noreply.github.com>
2022-11-07 10:52:36 +0100

    cleaned up the code

--- anscombe73.R
+++ anscombe73.R
@@ -1,51 +1,43 @@
 # CLEAR SCREEN AND WORKSPACE ----
-# From: https://www.njtierney.com/post/2020/06/01/tidy-anscombe/
-cat("\014"); rm(list = ls()); gc()
-# some font controls
-windowsFonts("Palatino" = windowsFont("Palatino Linotype"))
-# load packages (if pacman not installed, install first with install.packages("pacman"))
+rm(list = ls()); cat("\014"); gc(); 
+# SET DEFAULTS: display options, font and y axis label rotation
+options(digits = 8); options(scipen = 999);  options(max.print=10000); 
+windowsFonts("Palatino" = windowsFont("Palatino Linotype")); par(las=1) 
+# INSTALL PACMAN PACKAGE MANAGER IF NOT INSTALLED (Note: may need to disable windows firewall for packages to install)
+if (!"pacman" %in% installed.packages()){install.packages("pacman"); cat("pacman installed\n")}
+# LOAD REQUIRED PACKAGES 
 pacman::p_load(tidyverse, ggplot2, scales , extrafont, ggpubr, ggfortify)
 pacman::p_load(stats, ggpmisc)
 
+# check the loaded data
 head(anscombe)
 
+# re-arrange the data to be able to plot it using the format belwo
 tidy_anscombe <- anscombe %>%
   pivot_longer(cols = everything(),
                names_to = c(".value", "set"),
                names_pattern = "(.)(.)")
 tidy_anscombe$set = paste0("Data set ", tidy_anscombe$set)
-head(tidy_anscombe)
+# sort on data set, and then ascending in x
+tidy_anscombe     = arrange(tidy_anscombe, set, x)
+print(tidy_anscombe,n=100)
 
-# tidy_anscombe_summary <- tidy_anscombe %>%
-#   group_by(set) %>%
-#   summarise(across(.cols = everything(),
-#                    .fns = lst(min,max,median,mean,sd,var),
-#                    .names = "{col}_{fn}"))
-# 
-# tidy_anscombe_summary
- 
 ### -------------
 fnt = 12
-
 my.formula <- y ~ x
-ggplot(anscombe, aes(x = x, y = y)) + theme_bw() +
+ggplot(tidy_anscombe, aes(x = x, y = y)) + theme_bw() +
   facet_wrap(~set, scales='free', strip.position="bottom") +
   theme(axis.text.x = element_text(size = fnt, hjust  = 0.58, vjust = 0.5 , colour = "black"),
         axis.text.y = element_text(size = fnt, colour = "black"),
         text        = element_text(size = fnt, family = "Palatino"),
         axis.ticks.length = unit( -1.5, "mm"),
-        strip.text  = element_text(size = fnt), 
+        strip.text = element_text(size = fnt), 
         strip.background = element_rect( colour="black", fill="white", linetype="blank"),
         strip.placement = "outside",
         panel.spacing = unit(1, "lines") )  +
-    # facet_wrap(~set) +
-  # theme(strip.background = element_rect(colour="black", fill="white", 
-  #                                       size=0.005, linetype="solid")) + 
-  # geom_smooth(method = "lm", se = FALSE)
   geom_smooth(method = "lm", se = FALSE, formula = my.formula) +
   stat_poly_eq(formula = my.formula, 
                eq.with.lhs = c("italic(Y)~`=`~ "),
-               # eq.y.rhs = "~italic(Y)",
                eq.x.rhs = "~italic(X)",
                aes(label = paste(..eq.label.., ..rr.label.., sep = "*\",\"~~~")),
                parse = TRUE) +
@@ -53,4 +45,18 @@ ggplot(anscombe, aes(x = x, y = y)) + theme_bw() +
   scale_x_continuous(name = "X (independent variable)") +
   coord_cartesian( ylim = c(2.5, 12.5), xlim = c(5, 20) ) +
   grids(linetype = "dashed") + 
-  geom_point(size = 3)
+  geom_point(size=3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#EOF 
