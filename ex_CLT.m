21b3a21f60cc2a9eeb43c457d556e33130a8bc93
4db83 <84711454+4db83@users.noreply.github.com>
2024-11-13 10:15:20 +0100

    cleaned up figure

--- ex_CLT.m
+++ ex_CLT.m
@@ -1,21 +1,21 @@
 % example of CLT to replicate the plot on page 10 of EFF-02.
-clear;clc;clf;
+clear; clc 
 rng(123);   % fix the random number generator see
 n = 100;    % sample size
 S = 1e6;    % number of simulations just for the histograms
-% generate draws from a Bernoulli RV with success probabilty = p
+% generate draws from a Bernoulli RV with success probability = p
 p = 0.78;
 x = rand(S,n) < p;
 
-%%
+%
 clf;
-x2  = mean(x(:,1:2),2);
-x5  = mean(x(:,1:5),2);
-x25 = mean(x(:,1:25),2);
-x100 = mean(x(:,1:100),2);
+x2    = mean(x(:,1:2  ) ,2);
+x5    = mean(x(:,1:5  ) ,2);
+x25   = mean(x(:,1:25 ) ,2);
+x100  = mean(x(:,1:100) ,2);
 subplot(2,2,1)
 histogram(x2,25,'Normalization','probability');
-subtitle('$n=1$','Interpreter','Latex','Units','normalized','Position',[.5 .95],'FontSize',14)
+subtitle('$n=2$','Interpreter','Latex','Units','normalized','Position',[.5 .95],'FontSize',14)
 ylim([0 0.7])
 subplot(2,2,2)
 histogram(x5,25,'Normalization','probability');
